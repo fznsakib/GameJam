1433d0d052c605e0f7af29c502bba8d9be5fc7b9
fznsakib <fznsakib@gmail.com>
2018-11-19 21:38:04 +0000

    added flashlight toggle

---
+++ Assets/Scripts/lightToggle.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class lightToggle : MonoBehaviour
+    {
+        public SteamVR_Action_Boolean lightAction;
+
+        public SteamVR_Action_Vibration vibrator;
+
+        public Hand hand;
+
+        public GameObject light;
+
+        private void OnEnable()
+        {
+            if (hand == null)
+                hand = this.GetComponent<Hand>();
+
+            if (lightAction == null)
+            {
+                Debug.LogError("No plant action assigned");
+                return;
+            }
+
+            lightAction.AddOnChangeListener(OnActionChange, hand.handType);
+        }
+
+        private void OnDisable()
+        {
+            if (lightAction != null)
+                lightAction.RemoveOnChangeListener(OnActionChange, hand.handType);
+        }
+
+
+        private void OnActionChange(SteamVR_Action_In actionIn)
+        {
+            if (lightAction.GetStateDown(hand.handType))
+            {
+                light.SetActive(!light.activeSelf);
+                vibrator.Execute(0.0f, 0.03f, 200f, 0.1f, SteamVR_Input_Sources.LeftHand);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/shootingScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class shootingScript : MonoBehaviour
+    {
+        public SteamVR_Action_Boolean shootAction;
+
+        public SteamVR_Action_Vibration vibrator;
+
+        public Hand hand;
+
+        public GameObject GunTip;
+
+        private void OnEnable()
+        {
+            if (hand == null)
+                hand = this.GetComponent<Hand>();
+
+            if (shootAction == null)
+            {
+                Debug.LogError("No plant action assigned");
+                return;
+            }
+
+            shootAction.AddOnChangeListener(OnActionChange, hand.handType);
+        }
+
+        private void OnDisable()
+        {
+            if (shootAction != null)
+                shootAction.RemoveOnChangeListener(OnActionChange, hand.handType);
+        }
+
+
+        private void OnActionChange(SteamVR_Action_In actionIn)
+        {
+            if (shootAction.GetStateDown(hand.handType))
+            {
+                GunTip.GetComponent<PlayerShooting>().Shoot();
+                vibrator.Execute(0.0f, 0.1f, 100f, 0.99f, SteamVR_Input_Sources.RightHand);
+            }
+        }
+
+    }
+}
