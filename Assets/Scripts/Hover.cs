a74e2f1f0bccce1a51ba72d2ec51a91853ad684c
fznsakib <fznsakib@gmail.com>
2018-11-20 04:09:57 +0000

    hover added

---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour {
+
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    private Rigidbody Enemy;
+
+    Vector3 tempPos = new Vector3();
+    Vector3 posOffset = new Vector3();
+
+	// Use this for initialization
+	void Start () {
+        posOffset = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+	}
+
+}
