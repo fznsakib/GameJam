b5ff646f61775aa5202db0206fc5d557ae76ef54
fznsakib <fznsakib@gmail.com>
2018-11-20 07:02:36 +0000

    started merging projects

--- Assets/SteamVR_Input/ActionObjects/SteamVR_Input_ActionSet_default_In.cs
+++ Assets/SteamVR_Input/ActionObjects/SteamVR_Input_ActionSet_default_In.cs
@@ -34,5 +34,7 @@ namespace Valve.VR
         public Valve.VR.SteamVR_Action_Single Squeeze;
         
         public Valve.VR.SteamVR_Action_Boolean NewAction;
+        
+        public Valve.VR.SteamVR_Action_Boolean LightAction;
     }
 }
